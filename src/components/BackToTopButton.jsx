d589826e539076b656d7ddcee8da6526d51a6f7b
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-01-16 17:50:31 +0300

    edit top button scroll

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -5,7 +5,7 @@ const BackToTopButton = ({ showBackToTop, scrollToTop }) => {
     return (
         showBackToTop && (
             <motion.button
-                className="max-sm:w-24 w-auto max-sm:text-sm h-10 fixed max-sm:bottom-14 max-sm:right-0 max-sm:left-0 max-sm:mx-auto bottom-10 right-10 text-md flex items-center justify-center bg-gray-700 text-white p-4 rounded-full hover:bg-amber-400"
+                className="hidden max-sm:w-24 w-auto max-sm:text-sm h-10 fixed max-sm:bottom-20 max-sm:right-0 max-sm:left-0 max-sm:mx-auto bottom-10 right-10 text-md max-sm:flex items-center justify-center bg-gray-700 text-white p-4 rounded-full hover:bg-amber-400"
                 onClick={scrollToTop}
                 initial={{ opacity: 0, scale: 0 }}
                 animate={{ opacity: 1, scale: 1 }}
